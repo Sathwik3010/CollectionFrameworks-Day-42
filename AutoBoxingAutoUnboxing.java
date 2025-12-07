923dbe370e9ed4bd75885366352814ccc4f4e925
Dev Sathwik P <82132397+Sathwik3010@users.noreply.github.com>
2025-12-07 18:41:11 +0530

    AutoBoxing unboxing

---
+++ AutoBoxingAutoUnboxing.java
@@ -0,0 +1,19 @@
+package com.codegnan.collections;
+
+public class AutoBoxingAutoUnboxing {
+
+	public static void main(String[] args) {
+		int a = 5;
+		Integer obj = 5;
+		
+		System.out.println("Primitve type: "+a);
+		System.out.println("Wrapper object: "+obj);
+		
+		Double bobj = 50.9;
+		double b = 50.9;
+		
+		System.out.println("Wrapper object: "+bobj);
+		System.out.println("Primitve type: "+b);
+	}
+
+}
